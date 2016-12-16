75d33872bc7e3096f03ccf7ee30c05444750308b
Jared Wright <wrightj@ainfosec.com>
2016-12-16 12:18:43 -0700

    Test application for WS2812B driver and led helper functions. Currently, hardcoded to output on PORTB, Pin 0

--- Makefile
+++ Makefile
@@ -1,11 +1,32 @@
+# CROSS_COMPILE = avr-
+# CC := $(CROSS_COMPILE)gcc
+#
+# C_FILES := led.c blink.c
+# ASM_FILES := ws2812b.S
+# OBJ_FILES := $(notdir $(C_FILES:.c=.o))
+# OBJ_FILES += $(notdir $(ASM_FILES:.S=.o))
+#
+#
+# all: $(OBJ_FILES)
+#
+# $(OBJ_FILES): $(C_FILES) $(ASM_FILES)
+#
+# %.o: %.c
+#         $(CC) $(DBGFLAGS) -c $(INC) -o /$@ $<
+
 all:
 	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o blink.o blink.c
-	avr-gcc -mmcu=atmega328p blink.o -o blink
+	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o led.o led.c
+	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o ws2812b.o ws2812b.S
+	avr-gcc -mmcu=atmega328p blink.o led.o ws2812b.o -o blink
 	avr-objcopy -O ihex -R .eeprom blink blink.hex
 
 clean:
 	rm *.o *.hex blink
+	rm -r adafruit_neopixel
 
 flash_osx:
-	avrdude -F -V -c arduino -p ATMEGA328P -P /dev/cu.usbmodem14231 -b 115200 -U flash:w:blink.hex
+	avrdude -F -V -c arduino -p ATMEGA328P -P /dev/cu.usbmodem1411 -b 115200 -U flash:w:blink.hex
 
+neopixel:
+	avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o neopixel.o neopixel.S
--- blink.c
+++ blink.c
@@ -1,20 +1,48 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include "led.h"
  
 #define BLINK_DELAY_MS 1000
- 
+#define ONBOARD_LED_ON() (PORTB |= (1 << DDB5))
+#define ONBOARD_LED_OFF() (PORTB &= ~(1 << DDB5))
+
+void setup()
+{
+	// Setup pins 0 and 5 of PORTB as outputs
+	DDRB = (1 << DDB0) | (1 << DDB5);
+}
+
+void swirl(led_strand_t * leds)
+{
+	uint8_t i = 0;
+	if(!leds) return;
+	for(i = 0; i < 255; i++){
+		led_strand_set(leds, 0, i, 255 - i, 0);
+		led_strand_draw(leds);
+		_delay_ms(20);
+	}
+	for(i = 0; i < 255; i++){
+		led_strand_set(leds, 0, 255 - i, 0, i);
+		led_strand_draw(leds);
+		_delay_ms(20);
+	}
+	for(i = 0; i < 255; i++){
+		led_strand_set(leds, 0, 0, i, 255 - i);
+		led_strand_draw(leds);
+		_delay_ms(20);
+	}
+}
+
+void do_clock()
+{
+	led_strand_t * leds = led_strand_init(1);
+	while(1){
+		swirl(leds);
+	}	
+}
+
 int main (void)
 {
-	/* set pin 5 of PORTB for output*/
-	DDRB |= _BV(DDB5);
-
-	while(1) {
-		/* set pin 5 high to turn led on */
-		PORTB |= _BV(PORTB5);
-		_delay_ms(BLINK_DELAY_MS);
-
-		/* set pin 5 low to turn led off */
-		PORTB &= ~_BV(PORTB5);
-		_delay_ms(BLINK_DELAY_MS);
-	}
+	setup();
+	do_clock();
 }
