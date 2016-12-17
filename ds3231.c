c194b072d1627dfc7b901d816133f0df90ce307e
Jared Wright <wrightj@ainfosec.com>
2016-12-17 11:12:39 -0700

    Changed ds3231 driver to use modifications from last commit

--- ds3231.c
+++ ds3231.c
@@ -7,41 +7,24 @@
 // Toggle the clock between 12 and 24 hour mode
 // 		mode = 0 for 24 hour mode
 // 		mode = 1 for 12 hour mode
-int ds3231_set_mode(uint8_t mode)
+uint8_t ds3231_set_mode(uint8_t mode)
 {
-	if(mode != 0 || mode != 1) return -1;
-	return avr_i2c_write_byte(0x00 /* ?? */, 0x02, (mode << 6));
+	uint8_t value = ((mode & 0x01) << 6);
+	if(mode != 0 && mode != 1) return 1;
+	return avr_i2c_write(0x00 /* ?? */, 0x2, &value, 1);
 }
 
 // Set the internal clock of the DS3231 to the values of the given ds3231_time
-int ds3231_set_time(struct ds3231_time * t)
+uint8_t ds3231_set_time(struct ds3231_time * t)
 {
-	if(!t) return -1;
-
-	avr_i2c_write_byte(0x00 /* ?? */, 0, t->second);
-	avr_i2c_write_byte(0x00 /* ?? */, 1, t->minute);
-	avr_i2c_write_byte(0x00 /* ?? */, 2, t->hour);
-	avr_i2c_write_byte(0x00 /* ?? */, 3, t->day);
-	avr_i2c_write_byte(0x00 /* ?? */, 4, t->date);
-	avr_i2c_write_byte(0x00 /* ?? */, 5, t->month);
-	avr_i2c_write_byte(0x00 /* ?? */, 6, t->year);
-	return 0;
+	if(!t) return 1;
+	return avr_i2c_write(0x00 /* ?? */, 0, (uint8_t *)t, sizeof(struct ds3231_time));
 }
 
 // Read the current time into the given ds3231_time structure
-// Return 0 for success, 1 for failure
-int ds3231_get_time(struct ds3231_time * t)
+uint8_t ds3231_get_time(struct ds3231_time * t)
 {
-	if(!t) return -1;
-	struct ds3231_time temp = { 0 };
-
-	avr_i2c_read_byte(0x00 /* ?? */, 0, &(t->second));
-	avr_i2c_read_byte(0x00 /* ?? */, 1, &(t->minute));
-	avr_i2c_read_byte(0x00 /* ?? */, 2, &(t->hour));
-	avr_i2c_read_byte(0x00 /* ?? */, 3, &(t->day));
-	avr_i2c_read_byte(0x00 /* ?? */, 4, &(t->date));
-	avr_i2c_read_byte(0x00 /* ?? */, 5, &(t->month));
-	avr_i2c_read_byte(0x00 /* ?? */, 6, &(t->year));
-	return 0;
+	if(!t) return 1;
+	return avr_i2c_read(0x00 /* ?? */, 0, (uint8_t *)t, sizeof(struct ds3231_time)); 
 }
 
--- ds3231.h
+++ ds3231.h
@@ -4,18 +4,18 @@
 #include <stdint.h>
 
 __attribute__((packed)) struct ds3231_time {
-	uint8_t year;	// 0 - 99
-	uint8_t month;	// 1 - 12
-	uint8_t date;	// 1 - 31
-	uint8_t day;	// Weekday (1-7)
-	uint8_t hour;	// 1 - 12 + AM/PM (12hr mode) || 0 - 23 (24hr mode)
-	uint8_t minute;	// 0 - 59
 	uint8_t second;	// 0 - 59
+	uint8_t minute;	// 0 - 59
+	uint8_t hour;	// 1 - 12 + AM/PM (12hr mode) || 0 - 23 (24hr mode)
+	uint8_t day;	// Weekday (1-7)
+	uint8_t date;	// 1 - 31
+	uint8_t month;	// 1 - 12
+	uint8_t year;	// 0 - 99
 };
 
-int ds3231_set_mode(uint8_t mode);
-int ds3231_set_time(struct ds3231_time * t);
-int ds3231_get_time(struct ds3231_time * t);
+uint8_t ds3231_set_mode(uint8_t mode);
+uint8_t ds3231_set_time(struct ds3231_time * t);
+uint8_t ds3231_get_time(struct ds3231_time * t);
 
 #endif
 
