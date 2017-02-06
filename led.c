a3cfcc4bdbb194a5602b8d7150925e8d1179da9c
Jared Wright <wrightj@ainfosec.com>
2017-02-06 08:56:30 -0700

    Updated led and ws2812b driver to allow for initializtion on any port/pin, instead of a hard coded port and pin

--- led.c
+++ led.c
@@ -1,33 +1,58 @@
 #include "led.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+#include <avr/io.h>
 
-// Structure represents one WS2812B RGB LED
-// A WS2812B reads values in the order G-R-B, so they are orginized in
-// memory as such
-__attribute__((packed)) struct rgb_led { 
-	uint8_t g_val;
-	uint8_t r_val;
-	uint8_t b_val;
-};
-
-// Strucutre represents a chain of WS2812B RGB LEDs connected together in series
+//
+// Private declarations
+//
 struct led_strand {
-	struct rgb_led * led_buffer;
+	struct rgb * led_buffer;
 	uint16_t led_count;
+	volatile uint8_t * out_port;
+	uint8_t out_pin;
 };
 
-led_strand_t * led_strand_init(uint8_t count)
+// Low-level LED driver (ws2812b.S)
+extern void ws2812b_strand_draw(uint8_t * buffer, uint16_t size,
+		volatile uint8_t * out_port, uint8_t out_pin);
+
+//
+// Implementation
+//
+led_strand_t * led_strand_init(uint8_t count, volatile uint8_t * port, uint8_t pin)
 {
 	struct led_strand * leds = malloc(sizeof(struct led_strand));
-	if(!leds) return NULL;
+	if(!leds) goto error;
+	leds->led_buffer = malloc(sizeof(struct rgb) * count);
+	if(!leds->led_buffer) goto error;
 
 	leds->led_count = count;
-	leds->led_buffer = malloc(sizeof(struct rgb_led) * count);
+	leds->out_port = port;
+	leds->out_pin = 1 << pin;
+
 	led_strand_clear(leds);
 
+	// Set the specified port/pin as an output 
+	if(pin > 7) goto error;
+	if(port == &PORTA){
+		DDRA |= (1 << pin);
+	}else if(port == &PORTB){
+		DDRB |= 1 << pin;
+	}else if (port == &PORTC){
+		DDRC |= 1 << pin;
+	}else if(port == &PORTD){
+		DDRD |= 1 << pin;
+	}else{
+		goto error;
+	}
+
 	return leds;
+error:
+	led_strand_free(leds);
+	return NULL;	
 }
 
 void led_strand_free(led_strand_t * leds)
@@ -37,32 +62,37 @@ void led_strand_free(led_strand_t * leds)
 	free(leds);
 }
 
-int led_strand_set(led_strand_t * leds, uint8_t index, uint8_t r, uint8_t g, uint8_t b)
+int led_strand_set(led_strand_t * leds, uint8_t index, struct rgb * color)
 {
-	struct rgb_led * led = NULL;
+	struct rgb * led = NULL;
 
-	if(!leds) return -1;
+	if(!leds || !color) return -1;
 	if(index > leds->led_count) return -1;
 	
 	led = leds->led_buffer + index;
-	led->r_val = r;
-	led->g_val = g;
-	led->b_val = b;	
+	led->r = color->r;
+	led->g = color->g;
+	led->b = color->b;	
 	return 0;
 }
 
 void led_strand_clear(led_strand_t * leds)
 {
 	if(!leds || !leds->led_buffer) return;
-	memset(leds->led_buffer, 0, leds->led_count * sizeof(struct rgb_led));
+	memset(leds->led_buffer, 0, leds->led_count * sizeof(struct rgb));
 }
 
-extern void ws2812b_strand_draw(uint8_t * buffer, uint16_t size);
 int led_strand_draw(led_strand_t * leds)
 {
 	if(!leds || !leds->led_buffer) return -1;
 	ws2812b_strand_draw((uint8_t *)(leds->led_buffer),
-			leds->led_count * sizeof(struct rgb_led));
+			leds->led_count * sizeof(struct rgb), leds->out_port, leds->out_pin);
 	return 0;
+}
+
+uint8_t led_strand_get_count(led_strand_t * leds)
+{
+	if(!leds) return 0;
+	return leds->led_count;
 }
 
